f80588ebc18efab4191caf27e62f26e8a06e5dd2
Igor Dianov <igor@dianov.com>
2019-01-15 17:53:47 -0800

    fix: enable CORS policy

--- src/main/java/org/activiti/cloud/notifications/graphql/NotificationsGraphQLApplication.java
+++ src/main/java/org/activiti/cloud/notifications/graphql/NotificationsGraphQLApplication.java
@@ -18,6 +18,11 @@ package org.activiti.cloud.notifications.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NotificationsGraphQLApplication {
@@ -26,4 +31,21 @@ public class NotificationsGraphQLApplication {
         SpringApplication.run(NotificationsGraphQLApplication.class,
                 args);
     }
+    
+    @Configuration
+    public class FilterProvider {
+
+        @Bean
+        public WebMvcConfigurer corsConfiguration() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods(HttpMethod.GET.toString(), HttpMethod.POST.toString(),
+                                    HttpMethod.PUT.toString(), HttpMethod.DELETE.toString(), HttpMethod.OPTIONS.toString())
+                            .allowedOrigins("*");
+                }
+            };
+        }
+    }    
 }
